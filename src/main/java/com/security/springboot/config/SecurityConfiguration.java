d56b73856a06e3973142d6bc2c469f5c61de3cad
davi38 <davi4alves@gmail.com>
2022-10-27 01:26:24 -0300

    feat: Bcrypt and login

--- src/main/java/com/security/springboot/config/SecurityConfiguration.java
+++ src/main/java/com/security/springboot/config/SecurityConfiguration.java
@@ -1,9 +1,9 @@
 package com.security.springboot.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -19,17 +19,20 @@ public class SecurityConfiguration extends WebSecurityConfiguration {
 	@Autowired
 	private UserService userService;
 
-	@Bean
+
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
 		auth.setUserDetailsService(userService);
 		auth.setPasswordEncoder(passwordEncoder());
 		return auth;
+	}
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
 	}
 
 	protected void configure(HttpSecurity http) throws Exception {
