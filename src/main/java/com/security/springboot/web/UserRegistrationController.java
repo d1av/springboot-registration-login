95ba90d696bc573ab45d39726ca3b0a497111c5c
davi38 <davi4alves@gmail.com>
2022-10-26 22:50:20 -0300

    feat: new html page

--- src/main/java/com/security/springboot/web/UserRegistrationController.java
+++ src/main/java/com/security/springboot/web/UserRegistrationController.java
@@ -1,7 +1,10 @@
 package com.security.springboot.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.security.springboot.service.UserService;
@@ -18,6 +21,19 @@ public class UserRegistrationController {
 		this.userService = userService;
 	}
 	
+	@ModelAttribute("user")
+	public UserRegistrationDto userRegistrationDto() {
+		return new UserRegistrationDto();
+	}
+	
+	@GetMapping							//(Model model)
+	public String showRegistrationForm() {
+		//model.addAttribute("user", new UserRegistrationDto());
+		return "registration";
+	}
+	
+	
+	@PostMapping
 	public String registerUserAccount(@ModelAttribute("user")UserRegistrationDto registrationDto) {
 		userService.save(registrationDto);
 		return "redirect:/registration?success";
